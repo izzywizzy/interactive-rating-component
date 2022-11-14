2130e3c9f21bf2fe1420ecca7cb678019b122519
izzy chuks <ceziakonwa@gmail.com>
2022-11-14 11:22:27 +0100

    added js behaviour

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+// Hide and Show Rating form on clicked function
+$( "button" ).on( "click", function() {
+   $(".rating-container").hide();
+   $(".rating-container-thankyou").show();
+
+  
+   
+});
+
+// Change text to the selected item
+
+ $(".list-group-item").click(function () {
+    $(this).css("background-color","hsl(25, 97%, 53%)")
+    var selectedRating = ($(this).text());
+     $(".selected-rating").text("You selected " + selectedRating + " out of 5"); 
+    
+})
